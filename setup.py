75909ec7eefbbaddc27dff6abf434f603d48686a
Aaron Voelker <arvoelke@gmail.com>
2019-04-11 10:33:28 -0400

    fixup! Drop Python 2 support

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: Free for non-commercial use",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
